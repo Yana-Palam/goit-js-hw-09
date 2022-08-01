de0a8d182f4a47acee25a9ff9d7f60a03a085e5c
Yana-Palam <106307556+Yana-Palam@users.noreply.github.com>
2022-08-01 15:08:07 +0000

    Deploying to gh-pages from @ Yana-Palam/goit-js-hw-09@a582a39f2467ad7b4bb965377838260a1e7f9727 🚀

---
+++ 01-color-switcher.2e7f3953.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")},n=null;t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled=!1,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.2e7f3953.js.map
---
+++ 01-color-switcher.fe357898.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};let e=null;t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled=!1,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.fe357898.js.map
